196d5d9239d24c845766f42f27bc0c3224eaab33
OLAMIDE <ajaofaruq5@gmail.com>
2022-12-23 15:29:03 +0100

    I NEED A MACBOOK

---
+++ main.js
@@ -0,0 +1,8 @@
+function changeColor() {
+document.getElementById('button').style.background = 'green';
+setTimeout('changeColor2()', 2000)
+}
+{
+//   let changeBackground = document.createElement("style");
+//   changeBackground.style = "background:green";...0000000000
+    }
