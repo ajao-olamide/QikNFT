af8f33009c896000553cbe155a452abb609d2dfc
OLAMIDE <ajaofaruq5@gmail.com>
2023-01-07 11:37:25 +0100

    added focus to the website

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 function changeColor() {
-document.getElementById('button').style.background = 'green';
-setTimeout('changeColor2()', 2000)
+  document.getElementById("button").style.background = "green";
+  setTimeout("changeColor2()", 2000);
 }
 {
-//   let changeBackground = document.createElement("style");
-//   changeBackground.style = "background:green";...0000000000
-    }
+  let changeBackground = document.createElement("style");
+  changeBackground.style = "background:green";
+}
+
+let buttons = document.querySelectorAll(".different");
+
+// document.addEventListener("click", function (evt) {
+//   if (evt.target.classList.contains("different")) {
+//     buttons.forEach(function (button) {
+//       button.classList.remove("active");
+//     });
+//     evt.target.classList.add("active");
+//   }
+// });
